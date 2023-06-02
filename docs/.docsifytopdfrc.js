6c7dfe6627b379fd3b0b2c31a5397115959087a3
柚木鉉 <GloomyGhost@foxmail.com>
2023-06-02 11:23:36 +0800

    [docs] add gitignore for pdf convertor

---
+++ docs/.docsifytopdfrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  contents: [ "_sidebar.md" ], // array of "table of contents" files path
+  pathToPublic: "pdf/readme.pdf", // path where pdf will stored
+  pdfOptions: "", // reference: https://github.com/GoogleChrome/puppeteer/blob/master/docs/api.md#pagepdfoptions
+  removeTemp: true, // remove generated .md and .html or not
+  emulateMedia: "screen", // mediaType, emulating by puppeteer for rendering pdf, 'print' by default (reference: https://github.com/GoogleChrome/puppeteer/blob/master/docs/api.md#pageemulatemediamediatype)
+}
